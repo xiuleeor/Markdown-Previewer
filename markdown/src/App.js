14c22fe3192d3de7ba641af3f416f3ead5f138c7
Xiulee <131414233+xiuleeor@users.noreply.github.com>
2023-05-29 14:44:43 -0500

    Install markdown

--- markdown/src/App.js
+++ markdown/src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import { marked } from 'marked';
 
 function App() {
+  const [markdown, setMarkdown] = useState('');
+
+  const handleInputChange = (event) => {
+    setMarkdown(event.target.value);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Markdown Previewer</h1>
+      <div className="editor">
+        <textarea value={markdown} onChange={handleInputChange} />
+      </div>
+      <div className="preview" dangerouslySetInnerHTML={{ __html: marked(markdown) }} />
     </div>
   );
 }
